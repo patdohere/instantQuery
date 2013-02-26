b4a4d1287eda3ee572428f50629c90b376f5d0a6
Patrick Do <patdohere@gmail.com>
2013-02-25 17:08:52 -0800

    Added Model Examples

--- public/js/router.js
+++ public/js/router.js
@@ -1,7 +1,18 @@
 IQ.Router.map(function() {
-  this.route("index");
-  this.route("show");
+  this.resource('rooms', function() {
+    this.resource('room', {path:':path_id'}, function() {
+      this.resource('questions', function() {
+        this.resource('question', {path:':question_id'}, function() {
+          this.resource('answers', function() {
+            this.resource('answer', {path:':answer_id'}, function() {
+            })
+          })
+        })
+      })
+    })
+  })
 })
+
 
 IQ.ApplicationRoute = Ember.Route.extend({
 
@@ -11,8 +22,27 @@ IQ.IndexRoute = Ember.Route.extend({
 
 });
 
-IQ.ShowRoute = Ember.Route.extend({
+IQ.Rooms = Ember.Route.extend({
   model: function() {
-    return IQ.Person.find();
+    return IQ.Room.find();
   }
-});
+})
+IQ.Room = Ember.Route.extend({
+  
+})
+IQ.Questions = Ember.Route.extend({
+  model: function() {
+    return IQ.Question.find();
+  }
+})
+IQ.Question = Ember.Route.extend({
+  
+})
+IQ.Answers = Ember.Route.extend({
+  model: function() {
+    return IQ.Answer.find();
+  }
+})
+IQ.Answer = Ember.Route.extend({
+  
+})
