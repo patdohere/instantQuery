4eae57b192899eaf3287d648040d74eec9dfd956
Joseph Than <thanjat1@gmail.com>
2013-03-11 23:50:26 -0700

    Changed the Adapter
    Fun stuff.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function () {
-  app.set('port', process.env.PORT || 80);
+  app.set('port', process.env.PORT || 8080);
   app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
   app.use(express.bodyParser()),
   app.use(express.static(path.join(__dirname, 'public')));
@@ -28,3 +28,9 @@ app.post('/rooms/:id/questions', rooms.addRoom);
 app.get('/rooms/:id/questions/:id', rooms.findById);
 app.put('/rooms/:id/questions/:id', rooms.updateRoom);
 app.delete('/rooms/:id/questions/:id', rooms.deleteRoom);
+
+app.get('/rooms/:id/questions/:id/answers', rooms.findAll);
+app.post('/rooms/:id/questions/:id/answers', rooms.addRoom);
+app.get('/rooms/:id/questions/:id/answers/:id', rooms.findById);
+app.put('/rooms/:id/questions/:id/answers/:id', rooms.updateRoom);
+app.delete('/rooms/:id/questions/:id/answers/:id', rooms.deleteRoom);
